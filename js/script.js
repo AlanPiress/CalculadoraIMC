92a671b41a4a6090999705ab2dab3b0a9f777885
Alan Pires <alanpires511@gmail.com>
2021-09-16 17:18:54 -0300

    Desenvolvendo a função calculoImc - JS

---
+++ js/script.js
@@ -0,0 +1,10 @@
+calcular=addEventListener("click", calculoImc);
+
+function calculoImc (){
+    const altura= document.getElementById("altura").value;
+    const peso= document.getElementById("peso").value;
+    const calcular= document.getElementById("calcular");
+    const resultado= document.getElementById("resultado");
+    
+
+}
